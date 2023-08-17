810f482f3f0d8f572e28cdb258ea2070faa2e74e
Jan Xavier Virgen <54022069+janxavierv17@users.noreply.github.com>
2023-08-17 12:35:24 +1000

    chore: inject store in redux provider and adjust body css

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store/store.js";
+import { GlobalStyle } from "./Main.styles.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<GlobalStyle />
+			<App />
+		</Provider>
+	</React.StrictMode>
+);
